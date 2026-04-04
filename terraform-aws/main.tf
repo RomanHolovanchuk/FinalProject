d37938236dd2704ccce8a1d716769603892b83c5
Roman Holovanchuk <ro090986grs@gmail.com>
2026-04-04 23:23:11 +0300

    s3

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-roma"
+    key            = "gameshop/terraform.tfstate" # шлях до файлу всередині бакету
+    region         = "eu-north-1"
+    encrypt        = true                         # шифрування для безпеки
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 # --- Load Balancer ---
 resource "aws_lb" "main" {
   name               = "gameshop-alb"
